83a2f5cd41cc0a160386157d58125556e218ea79
mikejv36@gmail.com <mikejv36@gmail.com>
2024-03-28 15:58:51 -0400

    timer issues

--- floor.gd
+++ floor.gd
@@ -4,23 +4,39 @@ extends StaticBody2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$TimerLabel.visible = false
-	$Button.pressed.connect(_button_pressed)
+	$TypePrompt.visible = false
+	$TypeTimerLabel.visible = false
+	
+	$StartButton.pressed.connect(_start_button_pressed)
 	$Countdown.timeout.connect(_timer_stopped)
+	$TypeTimer.timeout.connect(_typetimer_stopped)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var timeLeft = $Countdown.get_time_left()
+	var timeLeft = $Countdown.get_time_left() + 1
 	$TimerLabel.text = "%01d" % timeLeft
+	
+	var timeLeftType = $TypeTimer.get_time_left() + 1
+	$TypeTimerLabel.text = "%01d" % timeLeftType
 
-# Called on button press
-func _button_pressed():
+# Called on start button press
+func _start_button_pressed():
 	$Countdown.start(5)
-	$Button.visible = false
+	$StartButton.visible = false
 	$TimerLabel.visible = true
-	print("Timer Starting")
+	$TypePrompt.visible = true
 
+#called on 1st timer stoppage
 func _timer_stopped():
 	$Countdown.stop()
-	$Button.visible = true
 	$TimerLabel.visible = false
-	print("Timer Stopped")
+	#start the typing test timer
+	$TypeTimer.start(30)
+	$TypeTimerLabel.visible = true
+	
+#called on typing timer stoppage
+func _typetimer_stopped():
+	$TypeTimer.stop()
+	$StartButton.visible = true
+	$TypeTimerLabel.visible = false
+	$TypePrompt.visible = false
